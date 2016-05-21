565185a5840657ccc6813acf55cb191447a95fa5
Administrator <Administrator@WIN-JH13BJM99UM>
2016-05-21 10:03:32 +0800

    add

--- Upload/UploadDemo.ashx.cs
+++ Upload/UploadDemo.ashx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -15,13 +16,23 @@ namespace Upload
         {
             context.Response.ContentType = "text/html";
     
-            //难道上传的文件
+            //拿到上传的文件
             HttpPostedFile file = context.Request.Files["imgFile"];
 
-            string path = "/upload/"+Guid.NewGuid().ToString()+file.FileName;
-            file.SaveAs(context.Request.MapPath(path));
-            string str = string.Format("<html><head></head><body><img src='{0}'/></body></html>", path);
-            context.Response.Write(str);
+            if (Path.GetExtension(file.FileName) == ".jpg")
+            {
+                string path = "/upload/" + Guid.NewGuid().ToString() + file.FileName;
+                file.SaveAs(context.Request.MapPath(path));
+                string str = string.Format("<html><head></head><body><img src='{0}'/></body></html>", path);
+                context.Response.Write(str);
+                context.Response.End();
+            }
+            else
+            {
+                context.Response.Write("非法文件");
+                context.Response.End();
+            }
+
 
         }
 
